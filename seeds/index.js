7892d91967033e938dba00fd14d212d1bb586942
dominik-matysek <dominik0matysek@gmail.com>
2023-11-07 13:38:50 +0100

    Added authorization and modified routes

--- seeds/index.js
+++ seeds/index.js
@@ -38,6 +38,7 @@ const seedDB = async () => {
         const random1000 = Math.floor(Math.random() * 1000);
         const price = Math.floor(Math.random()*20) +10;
         const camp = new Campground({
+            author: '654a17550915a329a1407d9c',
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
             title: `${sample(descriptors)} ${sample(places)}`,
             // image: 'https://unsplash.com/photos/lake-sorrounded-by-trees-4hhP-Ud4e84',
